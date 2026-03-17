c038641c2ef6af348a703dfd87c080fabb4088af
Daniil Knaub <andy.typing@mail.ru>
2026-03-17 10:20:34 +0800

    Added hierarchy of book's classes

---
+++ Books.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PTFourthLabWork
+{
+    public class Book
+    {
+
+    }
+    public class Dictionary : Book
+    {
+        public int pageCount = 250;
+        public int wordCount = 1000;
+        public string language_pair = "Английский-Русский";
+    }
+
+    public class RecipeBook : Book
+    {
+        public int pageCount = 250;
+        public int recipeCount = 200;
+        public string cuisine = "Китайская";
+    }
+    
+    public enum BiographiesType {autobiography, memoir, groupBiography, biography};
+    public class Biography : Book
+    {
+        public int pageCount = 400;
+        public string person = "Стив Джобс";
+        public BiographiesType biographyType = BiographiesType.biography;
+    }
+}
